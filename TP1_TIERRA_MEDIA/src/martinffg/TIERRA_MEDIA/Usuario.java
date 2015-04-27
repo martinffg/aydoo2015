c549f0a143c2151c7e2346b203f0a195a862dfea
martinffg <martinffg@gmail.com>
2015-04-27 02:53:39 -0300

    Commit TIERRA MEDIA Usuario Crear Test

---
+++ TP1_TIERRA_MEDIA/src/martinffg/TIERRA_MEDIA/Usuario.java
@@ -0,0 +1,69 @@
+package martinffg.TIERRA_MEDIA;
+
+public class Usuario {
+	
+	private String nombre;
+	private String password;
+	private double presupuesto;
+	private double tiempoDisponibleVisitas; // En Horas
+	private double velocidadTraslado; // En KM/H
+	private TipoAtraccion tipoAtraccionPreferida;
+	private PosicionGlobal posicionActual;
+	
+	public Usuario(String nombre_usuario,double presupuesto,int tiempoDisponible,
+					double velocidad,TipoAtraccion atraccion_pref){
+		
+		this.nombre = nombre_usuario;
+		this.password = "";
+		this.presupuesto = presupuesto;
+		this.tiempoDisponibleVisitas = tiempoDisponible;
+		this.velocidadTraslado = velocidad;
+		this.tipoAtraccionPreferida = atraccion_pref;
+		this.posicionActual= new PosicionGlobal(0,0,"Origen_coordenadas");
+
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public double getPresupuesto() {
+		return presupuesto;
+	}
+	public void setPresupuesto(double presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+	public double getTiempoDisponibleVisitas() {
+		return tiempoDisponibleVisitas;
+	}
+	public void setTiempoDisponibleVisitas(int tiempoDisponibleVisitas) {
+		this.tiempoDisponibleVisitas = tiempoDisponibleVisitas;
+	}
+	public double getVelocidadTraslado() {
+		return velocidadTraslado;
+	}
+	public void setVelocidadTraslado(double velocidadTraslado) {
+		this.velocidadTraslado = velocidadTraslado;
+	}
+	public TipoAtraccion getTipoAtraccionPreferida() {
+		return tipoAtraccionPreferida;
+	}
+	public void setTipoAtraccionPreferida(TipoAtraccion tipoAtraccionPreferida) {
+		this.tipoAtraccionPreferida = tipoAtraccionPreferida;
+	}
+	public PosicionGlobal getPosicionActual() {
+		return posicionActual;
+	}
+	public void setPosicionActual(PosicionGlobal posicionActual) {
+		this.posicionActual = posicionActual;
+	}
+
+}
---
+++ TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/UsuarioTest.java
@@ -0,0 +1,25 @@
+package martinffg.TIERRA_MEDIA;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class UsuarioTest {
+
+ @Test
+    public void crearUsuario(){
+        
+	 	// creo usuarioPrueba, con 10000 pesos, 6 Hs para recorrer, se mueve a 6 KM/H y prefiere las Aventuras
+	 	Usuario usuario = new Usuario("usuarioPrueba",10000,6,6,TipoAtraccion.AVENTURA); 
+	 	
+	 	Assert.assertEquals("usuarioPrueba", usuario.getNombre());
+	 	Assert.assertEquals("", usuario.getPassword());
+	 	Assert.assertEquals(6, usuario.getTiempoDisponibleVisitas(),0.01);
+	 	Assert.assertEquals(6, usuario.getVelocidadTraslado(),0.01);
+	 	Assert.assertSame(TipoAtraccion.AVENTURA, usuario.getTipoAtraccionPreferida());
+	 	Assert.assertEquals(0, usuario.getPosicionActual().getCoordenada_X());
+	 	Assert.assertEquals(0, usuario.getPosicionActual().getCoordenada_Y());  
+ 
+ 	}
+ 
+ 
+}
