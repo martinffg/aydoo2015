d6d527dea3a86468234bcdd928a9ec0a5042ba07
martinffg <martinffg@gmail.com>
2015-04-27 03:14:27 -0300

    Commit TIERRA MEDIA V5 Usuario Test 2

--- TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/UsuarioTest.java
+++ TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/UsuarioTest.java
@@ -11,6 +11,17 @@ public class UsuarioTest {
 	 	// creo usuarioPrueba, con 10000 pesos, 6 Hs para recorrer, se mueve a 6 KM/H y prefiere las Aventuras
 	 	Usuario usuario = new Usuario("usuarioPrueba",10000,6,6,TipoAtraccion.AVENTURA); 
 	 	
+	 	// valido que se haya creado el usuario
+	 	Assert.assertNotNull(usuario);
+ 
+ 	}
+ 
+ @Test
+ 	public void probarConsultaDatosUsuario(){
+	 	
+	 	Usuario usuario = new Usuario("usuarioPrueba",10000,6,6,TipoAtraccion.AVENTURA);
+	 
+	 	// valido que los datos ingresados en el creador por defecto sean correctos
 	 	Assert.assertEquals("usuarioPrueba", usuario.getNombre());
 	 	Assert.assertEquals("", usuario.getPassword());
 	 	Assert.assertEquals(6, usuario.getTiempoDisponibleVisitas(),0.01);
@@ -18,7 +29,7 @@ public class UsuarioTest {
 	 	Assert.assertSame(TipoAtraccion.AVENTURA, usuario.getTipoAtraccionPreferida());
 	 	Assert.assertEquals(0, usuario.getPosicionActual().getCoordenada_X());
 	 	Assert.assertEquals(0, usuario.getPosicionActual().getCoordenada_Y());  
- 
+	 
  	}
  
  
