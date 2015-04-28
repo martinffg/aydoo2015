f7b1d2800bc7df777c95e49dd339f73a2453c32a
martinffg <martinffg@gmail.com>
2015-04-28 01:04:01 -0300

    Commit V9 Tierra Media Atraccion V3

--- TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/AtraccionTest.java
+++ TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/AtraccionTest.java
@@ -36,6 +36,32 @@ public class AtraccionTest {
 	 	Assert.assertSame(TipoAtraccion.AVENTURA, atraccion.getTipoDeAtraccion());
  	 
 	}
+ 	
+ 	@Test
+ 	public void probarCambiosADatosAtraccion(){
+ 		
+ 		PosicionGlobal posicionMordor = new PosicionGlobal(10,10,"Posicion Mordor");
+ 	     
+	 	// creo atraccion nombre Mordor, en la pos 10,10, con costo 152,20, tiempo prom recorrido 122 de tipo Aventura
+	 	Atraccion atraccion = new Atraccion("Mordor",posicionMordor,152.20,1000,122.0,TipoAtraccion.AVENTURA);
+	 	
+ 		atraccion.setNombreAtraccion("GondorLand");
+ 		PosicionGlobal posicionNueva = new PosicionGlobal(30,30,"Posicion Nueva");
+ 		atraccion.setCoordenadasPosicion(posicionNueva);
+ 		atraccion.setCostoVisita(300.0);
+ 		atraccion.setCupoVisitantesDiarios(1500);
+ 		atraccion.setPromedioTiempoNecesarioParaVisitar(180.0);
+ 		atraccion.setTipoDeAtraccion(TipoAtraccion.PAISAJE);
+ 		
+ 		Assert.assertEquals("GondorLand", atraccion.getNombreAtraccion());
+	 	Assert.assertEquals(30, atraccion.getCoordenadasPosicion().getCoordenada_X());
+	 	Assert.assertEquals(30, atraccion.getCoordenadasPosicion().getCoordenada_Y());
+	 	Assert.assertEquals(300.0, atraccion.getCostoVisita(),0.01);
+	 	Assert.assertEquals(1500, atraccion.getCupoVisitantesDiarios());
+	 	Assert.assertEquals(180.0, atraccion.getPromedioTiempoNecesarioParaVisitar(),0.01);
+	 	Assert.assertSame(TipoAtraccion.PAISAJE, atraccion.getTipoDeAtraccion());
+ 		
+ 	}
  
   
 }
