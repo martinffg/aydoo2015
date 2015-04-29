7d5a159a1b42c20a3f5a227db34bfe638a875bee
martinffg <martinffg@gmail.com>
2015-04-29 14:15:57 -0300

    Commit V17 Tierra Media Promocion AXB Test V1

--- TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/TipoAtraccionTest.java
+++ TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/TipoAtraccionTest.java
@@ -7,19 +7,32 @@ import org.junit.Test;
 public class TipoAtraccionTest {
 	
 	@Test
-	public void probarTipoAtraccionTest(){
+	public void probar1TipoAtraccionTest(){
 		
 		TipoAtraccion tipo1 = TipoAtraccion.AVENTURA;
-		TipoAtraccion tipo2 = TipoAtraccion.PAISAJE;
-		TipoAtraccion tipo3 = TipoAtraccion.DEGUSTACION;
-		
-		
+				
 		Assert.assertEquals(TipoAtraccion.AVENTURA, tipo1);
-		Assert.assertEquals(TipoAtraccion.PAISAJE, tipo2);
-		Assert.assertEquals(TipoAtraccion.DEGUSTACION, tipo3);
-		Assert.assertNotSame(TipoAtraccion.AVENTURA, tipo3);
 		Assert.assertNotSame(TipoAtraccion.PAISAJE, tipo1);
+		
+	}
+	
+	@Test
+	public void probar2TipoAtraccionTest(){
+		
+		TipoAtraccion tipo2 = TipoAtraccion.PAISAJE;
+
+		Assert.assertEquals(TipoAtraccion.PAISAJE, tipo2);
 		Assert.assertNotSame(TipoAtraccion.DEGUSTACION, tipo2);
+		
+	}
+	
+	@Test
+	public void probar3TipoAtraccionTest(){
+		
+		TipoAtraccion tipo3 = TipoAtraccion.DEGUSTACION;
+
+		Assert.assertEquals(TipoAtraccion.DEGUSTACION, tipo3);
+		Assert.assertNotSame(TipoAtraccion.PAISAJE, tipo3);
 		
 	}
 
