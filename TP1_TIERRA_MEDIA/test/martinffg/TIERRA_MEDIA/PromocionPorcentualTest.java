1609c92d0589081509c16900f3b847fbd7e3f582
martinffg <martinffg@gmail.com>
2015-04-29 05:13:24 -0300

    Commit V15 TM Prom Porcent V3

--- TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/PromocionPorcentualTest.java
+++ TP1_TIERRA_MEDIA/test/martinffg/TIERRA_MEDIA/PromocionPorcentualTest.java
@@ -13,7 +13,7 @@ public class PromocionPorcentualTest {
     public void crearPromocionTest(){
 	 	
 	 	Date fechaHoy = new Date();
-	 	long fechaVence = fechaHoy.getTime()+864000; // Dentro de 10 días, uso formato TimeStamp
+	 	long fechaVence = fechaHoy.getTime()+864000; // Dentro de 10 dias, uso formato TimeStamp
 	 	PromocionPorcentual promocion = new PromocionPorcentual("Promo Porcentual Prueba",fechaVence,5.0); 
 	 	
 	 	// valido que se haya creado el promocion
@@ -50,12 +50,45 @@ public class PromocionPorcentualTest {
 		 itinerario.setAtracciones(atracciones);
 		
 		 Date fechaHoy = new Date();
-		 long fechaVence = fechaHoy.getTime()+864000; // Dentro de 10 días, uso formato TimeStamp
+		 long fechaVence = fechaHoy.getTime()+864000; // Dentro de 10 dias, uso formato TimeStamp
 		 PromocionPorcentual promocion = new PromocionPorcentual("Promo Porcentual Prueba",fechaVence,5.0);
 		 
 		 double descuentoObtenido = promocion.calcularDescuentoPromocionalAlItinerario(itinerario);
 		 
 		 Assert.assertEquals(20.0,descuentoObtenido,0.01);
+	}
+	
+	@Test
+	public void probarTotalConDescuentoPorcentualAItinerario(){
+		
+		 PosicionGlobal posicionMordor = new PosicionGlobal(10,10,"Posicion Mordor");
+		 PosicionGlobal posicionAldea = new PosicionGlobal(20,20,"Posicion Aldea");
+		 PosicionGlobal posicionGondor = new PosicionGlobal(30,30,"Posicion Gondor");
+	     
+		 // creo las distintas atracciones
+		 Atraccion atraccion1 = new Atraccion("Mordor",posicionMordor,152.20,1000,122.0,TipoAtraccion.AVENTURA);
+		 Atraccion atraccion2 = new Atraccion("Aldea",posicionAldea,47.80,1500,78.0,TipoAtraccion.DEGUSTACION);
+		 Atraccion atraccion3 = new Atraccion("Gondor",posicionGondor,200.00,500,100.0,TipoAtraccion.PAISAJE);
+		 
+		 // ahora genero la lista de atracciones
+		 List<Atraccion> atracciones = new ArrayList<Atraccion>();
+		 
+		 atracciones.add(atraccion1);
+		 atracciones.add(atraccion2);
+		 atracciones.add(atraccion3);
+		 
+		 // armo el itinerario de prueba
+		 Itinerario itinerario = new Itinerario("Itinerario de prueba");
+		 
+		 itinerario.setAtracciones(atracciones);
+		
+		 Date fechaHoy = new Date();
+		 long fechaVence = fechaHoy.getTime()+864000; // Dentro de 10 dias, uso formato TimeStamp
+		 PromocionPorcentual promocion = new PromocionPorcentual("Promo Porcentual Prueba",fechaVence,5.0);
+		 
+		 double pagoConDescuento = promocion.calcularCostoPromocionalDelItinerario(itinerario);
+		 
+		 Assert.assertEquals(380.0,pagoConDescuento,0.01);
 	}
   	 
 }
